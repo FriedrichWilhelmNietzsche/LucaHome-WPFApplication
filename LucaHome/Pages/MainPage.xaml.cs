ab664eeeb5a33ab2da97457bf0d8297031597c81
Jonas Schubert <guepardoapps@gmail.com>
2017-07-10 20:38:18 +0200

    Added prism, new layout MainPage
    added new usercontrol for mainpagecard
    added prism for commandbinding
    minor layout changes in settings

--- LucaHome/Pages/MainPage.xaml.cs
+++ LucaHome/Pages/MainPage.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using Common.Common;
 using Common.Tools;
+using Microsoft.Practices.Prism.Commands;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Navigation;
+using System;
+using System.ComponentModel;
+using OpenWeather.Service;
+using Data.Services;
 
 namespace LucaHome.Pages
 {
-    public partial class MainPage : Page
+    public partial class MainPage : Page, INotifyPropertyChanged
     {
         private const string TAG = "MainPage";
         private readonly Logger _logger;
 
         private readonly NavigationService _navigationService;
+        private readonly OpenWeatherService _openWeatherService;
+        private readonly TemperatureService _temperatureService;
 
         private readonly BirthdayPage _birthdayPage;
         private readonly MenuPage _menuPage;
@@ -25,7 +33,10 @@ namespace LucaHome.Pages
         public MainPage(NavigationService navigationService)
         {
             _logger = new Logger(TAG, Enables.LOGGING);
+
             _navigationService = navigationService;
+            _openWeatherService = OpenWeatherService.Instance;
+            _temperatureService = TemperatureService.Instance;
 
             _birthdayPage = new BirthdayPage(_navigationService);
             _menuPage = new MenuPage(_navigationService);
@@ -37,59 +48,108 @@ namespace LucaHome.Pages
             _wirelessSocketPage = new WirelessSocketPage(_navigationService);
 
             InitializeComponent();
+
+            SocketCard.ButtonAddCommand = new DelegateCommand(navigateToSocketAdd);
+            ShoppingListCard.ButtonAddCommand = new DelegateCommand(navigateToShoppingAdd);
+            BirthdayCard.ButtonAddCommand = new DelegateCommand(navigateToBirthdayAdd);
+            MovieCard.ButtonAddCommand = new DelegateCommand(navigateToMovieAdd);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs routedEventArgs)
         {
             _logger.Debug(string.Format("Page_Loaded with sender {0} with arguments {1}", sender, routedEventArgs));
             _navigationService.RemoveBackEntry();
+
+            // TODO Create and fix binding
+            WeatherCard.BottomTitleText.Text = _openWeatherService.City;
+            WeatherCard.BottomDataText.Text = string.Format("{0}°C", _openWeatherService.CurrentWeather.Temperature);
+
+            // TODO Create and fix binding
+            TemperatureCard.BottomTitleText.Text = _temperatureService.TemperatureList[0]?.Area;
+            TemperatureCard.BottomDataText.Text = _temperatureService.TemperatureList[0]?.TemperatureString;
         }
 
-        private void OpenWeather_Click(object sender, RoutedEventArgs routedEventArgs)
+        private void Page_Unloaded(object sender, RoutedEventArgs routedEventArgs)
         {
-            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, routedEventArgs));
-            _navigationService.Navigate(_weatherPage);
+            _logger.Debug(string.Format("Page_Unloaded with sender {0} with arguments {1}", sender, routedEventArgs));
+
         }
 
-        private void Socket_Click(object sender, RoutedEventArgs routedEventArgs)
+        private void SocketCard_MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
-            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, routedEventArgs));
+            _logger.Debug(string.Format("Received click of sender {0} with mouseButtonEventArgs {1}", sender, mouseButtonEventArgs));
             _navigationService.Navigate(_wirelessSocketPage);
         }
 
-        private void Birthday_Click(object sender, RoutedEventArgs routedEventArgs)
+        private void navigateToSocketAdd()
         {
-            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, routedEventArgs));
-            _navigationService.Navigate(_birthdayPage);
+            _logger.Debug("navigateToSocketAdd");
+            _navigationService.Navigate(new WirelessSocketAddPage(_navigationService));
         }
 
-        private void Movie_Click(object sender, RoutedEventArgs routedEventArgs)
+        private void WeatherCard_MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
-            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, routedEventArgs));
-            _navigationService.Navigate(_moviePage);
+            _logger.Debug(string.Format("Received click of sender {0} with mouseButtonEventArgs {1}", sender, mouseButtonEventArgs));
+            _navigationService.Navigate(_weatherPage);
         }
 
-        private void Temperature_Click(object sender, RoutedEventArgs routedEventArgs)
+        private void TemperatureCard_MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
-            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, routedEventArgs));
+            _logger.Debug(string.Format("Received click of sender {0} with mouseButtonEventArgs {1}", sender, mouseButtonEventArgs));
             _navigationService.Navigate(_temperaturePage);
         }
 
-        private void ShoppingList_Click(object sender, RoutedEventArgs routedEventArgs)
+        private void MenuCard_MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
-            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, routedEventArgs));
-            _navigationService.Navigate(_shoppingListPage);
-        }
-
-        private void Menu_Click(object sender, RoutedEventArgs routedEventArgs)
-        {
-            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, routedEventArgs));
+            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, mouseButtonEventArgs));
             _navigationService.Navigate(_menuPage);
         }
 
-        private void Settings_Click(object sender, RoutedEventArgs routedEventArgs)
+        private void ShoppingCard_MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
-            _logger.Debug(string.Format("Received click of sender {0} with arguments {1}", sender, routedEventArgs));
+            _logger.Debug(string.Format("Received click of sender {0} with mouseButtonEventArgs {1}", sender, mouseButtonEventArgs));
+            _navigationService.Navigate(_shoppingListPage);
+        }
+
+        private void navigateToShoppingAdd()
+        {
+            _logger.Debug("navigateToShoppingAdd");
+            _navigationService.Navigate(new ShoppingEntryAddPage(_navigationService));
+        }
+
+        private void BirthdayCard_MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            _logger.Debug(string.Format("Received click of sender {0} with mouseButtonEventArgs {1}", sender, mouseButtonEventArgs));
+            _navigationService.Navigate(_birthdayPage);
+        }
+
+        private void navigateToBirthdayAdd()
+        {
+            _logger.Debug("navigateToBirthdayAdd");
+            _navigationService.Navigate(new BirthdayAddPage(_navigationService));
+        }
+
+        private void MovieCard_MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            _logger.Debug(string.Format("Received click of sender {0} with mouseButtonEventArgs {1}", sender, mouseButtonEventArgs));
+            _navigationService.Navigate(_moviePage);
+        }
+
+        private void navigateToMovieAdd()
+        {
+            _logger.Debug("navigateToMovieAdd");
+            _navigationService.Navigate(new MovieAddPage(_navigationService));
+        }
+
+        private void SettingsCard_MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            _logger.Debug(string.Format("Received click of sender {0} with mouseButtonEventArgs {1}", sender, mouseButtonEventArgs));
             _navigationService.Navigate(_settingsPage);
         }
     }
---
+++ LucaHome/UserControls/MainPageCard.xaml.cs
@@ -0,0 +1,176 @@
+﻿using Common.Tools;
+using Microsoft.Practices.Prism.Commands;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LucaHome.UserControls
+{
+    public partial class MainPageCard : UserControl, INotifyPropertyChanged
+    {
+        private const string TAG = "MainPageCard";
+        private readonly Logger _logger;
+
+        // Help: https://social.msdn.microsoft.com/Forums/vstudio/en-US/8a1648fc-ddd7-427b-a7cd-99f04c506b7c/a-binding-cannot-be-set-on-the-text-property-of-type-webhyperlink-a-binding-can-only-be-set?forum=wpf
+        private static readonly DependencyProperty BottomTitleProperty = DependencyProperty.Register("BottomTitle", typeof(string), typeof(MainPageCard));
+        private static readonly DependencyProperty BottomDataProperty = DependencyProperty.Register("BottomData", typeof(string), typeof(MainPageCard));
+
+        private string _title;
+        private string _description;
+
+        private Uri _cardImage;
+
+        // Help: https://stackoverflow.com/questions/13838884/how-to-bind-a-command-in-wpf
+        private ICommand _buttonAddCommand;
+        private Visibility _buttonAddVisibility;
+
+        private string _bottomTitle;
+        private string _bottomData;
+
+        public MainPageCard()
+        {
+            _logger = new Logger(TAG);
+
+            _cardImage = new Uri("/Common;component/Assets/Wallpaper/wallpaper.png", UriKind.Relative);
+
+            _title = "Example";
+            _description = "Lorem ipsum...";
+
+            _buttonAddCommand = new DelegateCommand(dummyCommand);
+            _buttonAddVisibility = Visibility.Collapsed;
+
+            _bottomTitle = string.Empty;
+            _bottomData = string.Empty;
+
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Uri CardImage
+        {
+            get
+            {
+                _logger.Debug(string.Format("Get CardImage: {0}", _cardImage));
+                return _cardImage;
+            }
+            set
+            {
+                _cardImage = value;
+                _logger.Debug(string.Format("Set CardImage: {0}", _cardImage));
+                OnPropertyChanged("CardImage");
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                _logger.Debug(string.Format("Get Title: {0}", _title));
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                _logger.Debug(string.Format("Set Title: {0}", _title));
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                _logger.Debug(string.Format("Get Description: {0}", _description));
+                return _description;
+            }
+            set
+            {
+                _description = value;
+                _logger.Debug(string.Format("Set Description: {0}", _description));
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public ICommand ButtonAddCommand
+        {
+            get
+            {
+                _logger.Debug(string.Format("Get ButtonAddCommand: {0}", _buttonAddCommand));
+                return _buttonAddCommand;
+            }
+            set
+            {
+                _buttonAddCommand = value;
+                _logger.Debug(string.Format("Set ButtonAddCommand: {0}", _buttonAddCommand));
+                OnPropertyChanged("ButtonAddCommand");
+            }
+        }
+
+        private void dummyCommand()
+        {
+            _logger.Error("Please bind a command to this button!");
+        }
+
+        public Visibility ButtonAddVisibility
+        {
+            get
+            {
+                _logger.Debug(string.Format("Get ButtonAddVisibility: {0}", _buttonAddVisibility));
+                return _buttonAddVisibility;
+            }
+            set
+            {
+                _buttonAddVisibility = value;
+                _logger.Debug(string.Format("Set ButtonAddVisibility: {0}", _buttonAddVisibility));
+                OnPropertyChanged("ButtonAddVisibility");
+            }
+        }
+
+        public string BottomTitle
+        {
+            get
+            {
+                _logger.Debug(string.Format("Get BottomTitle: {0}", _bottomTitle));
+                return _bottomTitle;
+            }
+            set
+            {
+                _bottomTitle = value;
+                _logger.Debug(string.Format("Set BottomTitle: {0}", _bottomTitle));
+                OnPropertyChanged("BottomTitle");
+            }
+        }
+
+        public string BottomData
+        {
+            get
+            {
+                _logger.Debug(string.Format("Get BottomData: {0}", _bottomData));
+                return _bottomData;
+            }
+            set
+            {
+                _bottomData = value;
+                _logger.Debug(string.Format("Set BottomData: {0}", _bottomData));
+                OnPropertyChanged("BottomData");
+            }
+        }
+
+        private void MainPageCardContent_MouseEnter(object sender, MouseEventArgs mouseEventArgs)
+        {
+            Mouse.OverrideCursor = Cursors.Hand;
+        }
+
+        private void MainPageCardContent_MouseLeave(object sender, MouseEventArgs mouseEventArgs)
+        {
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+    }
+}
